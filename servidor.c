0c21bbcf01a81b0f6cf105f259df956335509829
Simon Oroño <sron611@gmail.com>
2014-06-17 15:15:57 -0430

    Agregado el código de verificar tablero y el archivo .gitignore

--- servidor.c
+++ servidor.c
@@ -1,5 +1,57 @@
 #include "proyecto.h"
 
+int verificar_tablero(tablero* tab)
+{
+    int full = 1;
+    int i;
+    int* column = tab->valores[0]; //Primera columna
+    int diagonal, cdiagonal;
+    diagonal = tab->valores[0][0]; //Empiezan las diagonales
+    cdiagonal = tab->valores[0][2];
+
+    for(i = 0; i < 3; i++)
+    {
+        if((tab->valores[i][0] == tab->valores[i][1]) &&
+                (tab->valores[i][0] == tab->valores[i][2]) &&
+                (tab->valores[i][0] != -1))
+            return tab->valores[i][0]; //Detectar linea horizontal
+        else
+        {
+            if((tab->valores[i][0] == -1) ||
+                    (tab->valores[i][1] == -1) ||
+                    (tab->valores[i][2] == -1))
+                full = 0; //Se puede continuar
+
+            if(i != 0)
+            {
+                diagonal = (diagonal == tab->valores[i][i]) ? diagonal : -1; //Verificar diagonales
+                cdiagonal = (cdiagonal == tab->valores[i][2-i]) ? cdiagonal : -1;
+
+                //Una columna se vuelve -1 si ya se perdió la victoria
+                if(column[0] != tab->valores[i][0]) column[0] = -1;
+                if(column[1] != tab->valores[i][1]) column[1] = -1;
+                if(column[2] != tab->valores[i][2]) column[2] = -1;
+            }
+        }
+    }
+
+    //Buscar ganador
+    if(diagonal != -1) return diagonal;
+    else if(cdiagonal != -1) return cdiagonal;
+    else
+    {
+        for(i = 0; i < 3; i++)
+        {
+            if(column[i] != -1) return column[i];
+        }
+    }
+
+    if(full)
+        return 2;
+    else
+        return -1;
+}
+
 int main(void)
 {
     mensaje msg;
